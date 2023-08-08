cbf4401c0e357ea36ef7d5b3fcfde706b58cdfca
christian sanabria <ckevin89@hotmail.com>
2023-08-08 10:01:44 -0500

    update

--- whiteboard.py
+++ whiteboard.py
@@ -34,3 +34,25 @@ def solution(name):
     return first_letter
 
 
+def solution(name):
+    vowels = ['a','e','i','o','u']
+    if(len(name)) <4:
+        return "Error: Name too short"
+
+    if name[2] in vowels:
+        return name[:4]
+    else:
+        return name[:3]
+    
+
+    def solution(str):
+    vowels = ['a','e','i','o','u']
+    consonants = ['b','c','d','f','g','h','j','k','l','m','n','o','p','q','r','s','t','v','w','x','y','z']
+    empty_list = []
+    if len(str) < 4:
+        return "Error: Name too short"
+    for index, value in enumerate(str):
+        if index == 2 and value.lower() in vowels:
+            return str[:index+2]
+        elif index == 2 and value.lower() in consonants:
+            return str[:index+1]
